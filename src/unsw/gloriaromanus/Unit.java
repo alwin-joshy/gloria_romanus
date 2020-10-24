636a7a538aba321bdde5f0acb315ac57a22581c6
David Nguyen <z5259636@ad.unsw.edu.au>
2020-10-24 14:41:13 +1100

    added method to update roads

--- src/unsw/gloriaromanus/Unit.java
+++ src/unsw/gloriaromanus/Unit.java
@@ -14,20 +14,20 @@ import org.json.JSONObject;
  */
 public abstract class Unit implements Serializable{
     private int numTroops;  // the number of troops in this unit (should reduce based on depletion)
-    private int range;  // range of the unit
+    private boolean ranged;  // range of the unit
     private int armour;  // armour defense
     private int morale;  // resistance to fleeing
     private int speed;  // ability to disengage from disadvantageous battle
     private int attack;  // can be either missile or melee attack to simplify. Could improve implementation by differentiating!
-    private int defenseSkill;  // skill to defend in battle. Does not protect from arrows!
-    private int shieldDefense; // a shield
+    private int defenceSkill;  // skill to defend in battle. Does not protect from arrows!
+    private int shieldDefence; // a shield
     private int baseCost;
     private int trainingTime;
-    private int smithLevel; 
+    private int smithLevel;
 
     public Unit(String filename){
         // TODO = obtain these values from the file for the unit
-        JSONObject json = new JSONObject("units/"+filename)
+        JSONObject json = new JSONObject("units/" + filename + ".json");
         numTroops = json.getInt("numTroops");
         ranged = json.getBoolean("ranged");
         armour = json.getInt("armour");
@@ -36,7 +36,7 @@ public abstract class Unit implements Serializable{
         attack = json.getInt("attack");
         defenceSkill = json.getInt("defenceSkill");
         shieldDefence = json.getInt("shieldDefence");
-        baseCose
+        baseCost = json.getInt("baseCost");
     }
 
     public int getNumTroops(){
