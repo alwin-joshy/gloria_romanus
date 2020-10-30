d729efd64fb37eb7d32fcebb40394be38e2b3256
Alwin Joshy <a.joshy@student.unsw.edu.au>
2020-10-30 17:32:09 +1100

    fixed some errors

--- src/unsw/gloriaromanus/BattleResolver.java
+++ src/unsw/gloriaromanus/BattleResolver.java
@@ -5,8 +5,4 @@ import java.util.Random;
 
 public interface BattleResolver {
     public boolean battle(Province attacking, ArrayList<Unit> attackingArmy, Province defending, ArrayList<Unit> defendingArmy);
-<<<<<<< HEAD
-
-=======
->>>>>>> b2a88603864968f5dad4faee9c5ac36d22207ed7
 }
