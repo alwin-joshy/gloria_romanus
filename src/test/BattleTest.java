777de69eae75cc835aa32ae386eb036b2b7fec0e
David Nguyen <z5259636@ad.unsw.edu.au>
2020-11-01 10:40:52 +1100

    adjusted string contents

--- src/test/BattleTest.java
+++ src/test/BattleTest.java
@@ -18,4 +18,23 @@ import unsw.gloriaromanus.*;
 
 public class BattleTest {
     private String initialOwnership = "{\r\n    \"Gaul\": [\r\n        \"A\",\r\n        \"B\"\r\n    ],\r\n    \"Rome\": [\r\n        \"C\",\r\n        \"D\"\r\n    ]\r\n}";
+    private String landlockedString = "[]";
+    private String adjacencyString = "{\r\n    \"A\": {\r\n        \"B\": true\r\n    },\r\n    \"B\": {\r\n        \"A\": true,\r\n        \"C\": true\r\n    },\r\n    \"C\": {\r\n        \"B\": true,\r\n        \"D\": true\r\n    },\r\n    \"D\": {\r\n        \"C\": true\r\n    }\r\n}";
+
+    public void initialSetup(Game g) throws IOException {
+        JSONObject ownership = new JSONObject(initialOwnership);
+        JSONArray landlocked = new JSONArray(landlockedString);
+        JSONObject adjacencyMap = new JSONObject(adjacencyString);
+        g.initialiseGame(ownership, landlocked, adjacencyMap);
+        g.selectFaction("Gaul");
+        g.startGame();
+        Faction gaul = g.getFactions().get(0);
+        gaul.setTreasury(10000);
+        gaul.getNthProvince(4).build(new TroopProductionBuilding(gaul));
+        g.endTurn();
+        gaul.getNthProvince(4).build(gaul.getNthProvince(4).getTroopProductionBuilding());
+        g.endTurn();
+        g.endTurn();
+    }
+
 }
