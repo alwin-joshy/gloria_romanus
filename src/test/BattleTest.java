ae14a6ab48e53d8ea18d1134d4a3bdfeabeccd15
David Nguyen <z5259636@ad.unsw.edu.au>
2020-10-31 21:30:12 +1100

    fixed movement test

---
+++ src/test/BattleTest.java
@@ -0,0 +1,21 @@
+package test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import org.json.JSONObject;
+import org.json.JSONArray;
+import org.junit.jupiter.api.Test;
+
+import unsw.gloriaromanus.*;
+
+public class BattleTest {
+    
+}
