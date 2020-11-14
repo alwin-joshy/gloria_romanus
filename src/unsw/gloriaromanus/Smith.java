572a31d70fab0f9bf447015cc81507ad9069f6e8
Adam McCabe <adam.mccabe2000@gmail.com>
2020-11-14 17:29:17 +1100

    Adjusting Description for front end

--- src/unsw/gloriaromanus/Smith.java
+++ src/unsw/gloriaromanus/Smith.java
@@ -6,7 +6,7 @@ public class Smith extends Infrastructure{
         setBaseContructionTime(2);
         setBaseCost(60);
         setName("Smith");
-        setDescription("Each Smith upgrade will give in-battle buff to troops depending on level; Level 1: EnemyDamageDebuff >= 1  Level 2: EnemyDamageDebuff-0.5 >= 1 + AlliedSpeedBuff0.8  Level 3: AlliedAttackBuff*1.2 + AlliedMoraleBuff 1.1  Level 4: EnemyMoraleDebuff = 0.8 + EnemyMissileDebuff-10%.");
+        setDescription("Each Smith upgrade will give in-battle buff to troops depending on level; \nLevel 1: EnemyDamageDebuff >= 1  \nLevel 2: EnemyDamageDebuff-0.5 >= 1 + AlliedSpeedBuff0.8  \n Level 3: AlliedAttackBuff*1.2 + AlliedMoraleBuff 1.1 \n Level 4: EnemyMoraleDebuff = 0.8 + EnemyMissileDebuff-10%.");
     }
 
     public void levelUp() {
--- src/unsw/gloriaromanus/Walls.java
+++ src/unsw/gloriaromanus/Walls.java
@@ -11,7 +11,7 @@ public class Walls extends Infrastructure {
         setBaseContructionTime(2);
         setBaseCost(120);
         setName("Walls");
-        setDescription("Each wall upgrade will provide a buffer to depending troop and can be upgraded into; Level 1 : Walls  Level 2: TwoArcherTowers Level 3: OneBallistaTower + TwoArcherTowers Level 4: ThreeBallistaTowers.");
+        setDescription("Each wall upgrade will provide a buffer to depending troop and can be upgraded into; \nLevel 1 : Walls  \nLevel 2: TwoArcherTowers \nLevel 3: OneBallistaTower + TwoArcherTowers \nLevel 4: ThreeBallistaTowers.");
         currentLevel = null;
     }
 
