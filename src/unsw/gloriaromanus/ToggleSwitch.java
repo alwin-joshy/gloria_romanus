cc6bd81154e11bdcb54504b280f0b75e241dcaa6
Alwin Joshy <a.joshy@student.unsw.edu.au>
2020-11-15 13:29:51 +1100

    Replaced toggle button with toggle switch and implemented on frontend

---
+++ src/unsw/gloriaromanus/ToggleSwitch.java
@@ -0,0 +1,61 @@
+package unsw.gloriaromanus;
+
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+
+
+public class ToggleSwitch extends HBox {
+	
+	private final Label label = new Label();
+	private final Button button = new Button();
+	private SimpleBooleanProperty selected = new SimpleBooleanProperty(false);
+	public SimpleBooleanProperty selectedProperty() { return selected; }
+	public boolean isSelected() {return selected.getValue();}
+	
+	private void init() {
+		label.setText("OFF");
+		getChildren().addAll(label, button);	
+		button.setOnAction((e) -> {
+			selected.set(!selected.get());
+		});
+		label.setOnMouseClicked((e) -> {
+			selected.set(!selected.get());
+		});
+		setStyle();
+		bindProperties();
+	}
+	
+	private void setStyle() {
+		//Default Width
+		setWidth(80);
+		label.setAlignment(Pos.CENTER);
+		setStyle("-fx-background-color: grey; -fx-text-fill:black; -fx-background-radius: 4;");
+		setAlignment(Pos.CENTER_LEFT);
+	}
+	
+	private void bindProperties() {
+		label.prefWidthProperty().bind(widthProperty().divide(2));
+		label.prefHeightProperty().bind(heightProperty());
+		button.prefWidthProperty().bind(widthProperty().divide(2));
+		button.prefHeightProperty().bind(heightProperty());
+	}
+	
+	public ToggleSwitch() {
+		init();
+		selected.addListener((a,b,c) -> {
+			if (c) {
+                		label.setText("ON");
+                		setStyle("-fx-background-color: green;");
+                		label.toFront();
+            		}
+            		else {
+            			label.setText("OFF");
+        			setStyle("-fx-background-color: red;");
+                		button.toFront();
+            		}
+		});
+	}
+}
